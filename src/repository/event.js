4916fc199df6a52790bcf73f138a4563a7e1a999
NebelFox <mashtaliaryaroslav@gmail.com>
2022-11-19 13:22:15 +0200

    Update events repo for new schema

--- src/repository/event.js
+++ src/repository/event.js
@@ -1,20 +1,35 @@
 const {db} = require("./db");
 
-const createEvent = event => db('events').insert({
+const createEvent = (event, userCreatorId) => db('events.event').insert({
+    user_creator_id: userCreatorId,
     title: event.title,
-    content: event.content,
-    start_date: event.startDate,
-    duration: event.duration,
+    description: event.description,
+    department_id: event.departmentId,
+    starts_at: event.startsAt,
+    ends_at: event.endsAt
 }).returning('id');
 
 const findAll = () => {
-    return db('events').select(['id', 'title', 'content', 'start_date as startDate', db.raw('CAST(duration AS VARCHAR)')]);
+    return db({e: 'events.event'})
+        .leftJoin('departments.department as d', 'e.department_id', '=', 'd.id')
+        .leftJoin('departments.faculty as f', 'd.faculty_id', '=', 'f.id')
+        .select([
+            'e.id', 
+            'e.user_creator_id as userCreatorId', 
+            'e.title',
+            'e.description', 
+            'e.department_id as departmentId',
+            'd.name as departmentName',
+            'f.id as facultyId',
+            'f.name as facultyName',
+            'e.starts_at as startsAt', 
+            'e.ends_at as endsAt',
+            'e.created_at as createdAt'
+        ]);
 }
 
 const findById = id => {
-    return db('events').select(['id', 'title', 'content', 'start_date as startDate', db.raw('CAST(duration AS VARCHAR)')])
-        .where({id})
-        .first()
+    return findAll().where({id}).first();
 }
 
 module.exports = {
