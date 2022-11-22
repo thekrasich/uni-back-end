981caf8ab30b17373ca393e311bdf7aa16fb52f9
NebelFox <mashtaliaryaroslav@gmail.com>
2022-11-22 11:41:15 +0200

    Update POST /events to require admin role

--- src/routes/event.js
+++ src/routes/event.js
@@ -5,11 +5,11 @@ const {body} = require('express-validator');
 
 const errorHandler = require("./error-handler");
 
-const {authorizeUser} = require("../service/auth");
+const {authorizeUser, requireRole} = require("../service/auth");
 const eventService = require('../service/event');
 
-router.post('/events', authorizeUser,
-    body('title').isLength({min: 10, max: 128}),
+router.post('/events', authorizeUser, requireRole(2),
+    body('title').isLength({min: 5, max: 128}),
     body('description').isLength({min: 20, max: 100000}),
     body('departmentId').isInt().toInt(),
     body('startsAt').isISO8601().toDate(),
