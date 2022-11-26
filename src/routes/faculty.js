c7de8f8b94b9019b54812ab4f3cad39cf1ab24d5
NebelFox <mashtaliaryaroslav@gmail.com>
2022-11-26 12:42:57 +0200

    Add faculties get by id and post routes

--- src/routes/faculty.js
+++ src/routes/faculty.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-const errorHandler = require("./error-handler");
+const { body } = require('express-validator');
+
+const { auth, authRole, errorHandler } = require("./../middleware");
 
 const facultyService = require('../services/faculty');
 
-router.get('/faculties', errorHandler(facultyService.findAll));
+router.post('/faculties',
+  authRole(2),
+  body('name'),
+  errorHandler(facultyService.create));
+
+router.get('/faculties', auth, errorHandler(facultyService.findAll));
+router.get('/faculties/:id([1-9][0-9]+|[1-9])', auth, errorHandler(facultyService.findById));
 
 module.exports = router;
