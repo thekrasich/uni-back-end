7fcc1d5a9586583f9222f000b0db584adb2cb8ae
NebelFox <mashtaliaryaroslav@gmail.com>
2022-11-26 12:37:21 +0200

    Update auth pre-handler location to middleware.js

---
+++ src/middleware.js
@@ -0,0 +1,62 @@
+const jwt = require('jsonwebtoken');
+
+const jwtSecret = 'test';
+
+const AUTH_TOKEN_HEADER = "Authorization";
+const TOKEN_PREFIX = "Bearer";
+
+// const authRole = (minRequiredRole = 1) => (req, res, next) => {
+//   const authHeader = req.header(AUTH_TOKEN_HEADER);
+//
+//   if (!authHeader) {
+//     res.status(401).send({ errorMessage: 'Missed Authorization header' });
+//     return;
+//   }
+//
+//   const authHeaderParts = authHeader.split(' ');
+//   if (authHeaderParts.length !== 2 || TOKEN_PREFIX !== authHeaderParts[0]) {
+//     res.status(401).send({ errorMessage: 'Invalid format of Auth header!' });
+//     return;
+//   }
+//
+//   const jwtToken = authHeaderParts[1];
+//
+//   try {
+//     jwt.verify(jwtToken, jwtSecret);
+//   } catch (e) {
+//     res.status(401).send({ errorMessage: 'Invalid Auth token!' });
+//     return;
+//   }
+//
+//   const { userId, roleId } = jwt.decode(jwtToken);
+//
+//   if (roleId < minRequiredRole) {
+//     res.status(403).send({ errorMessage: 'Your role has not enough rights for such request' });
+//     return;
+//   }
+//
+//   req.user = { id: userId, roleId };
+//
+//   next();
+// }
+//
+// const auth = authRole(1);
+
+const auth = (req, res, next) => next();
+const authRole = (minRequiredRole) => (req, res, next) => next();
+
+const errorHandler = routerCallBack => {
+  return async (req, res, next) => {
+    try {
+      await routerCallBack(req, res, next);
+    } catch (e) {
+      res.status(500).send({ errorMessage: `${e}` });
+    }
+  }
+};
+
+module.exports = {
+  authRole,
+  auth,
+  errorHandler
+};
