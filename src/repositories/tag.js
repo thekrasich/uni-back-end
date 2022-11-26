4e4573312e22764d16b67ee71646d5b0be438186
NebelFox <mashtaliaryaroslav@gmail.com>
2022-11-26 12:41:35 +0200

    Add create method to tag repo

--- src/repositories/tag.js
+++ src/repositories/tag.js
@@ -1,33 +1,33 @@
-const {db} = require("./db");
+const { db } = require("./db");
 
-const createTag = tag => {
-    return db('events.tag').insert({
-        name: tag.name,
-        color: tag.color
-    }).returning('id');
+const create = tag => {
+  return db('events.tag').insert({
+    name: tag.name,
+    color: tag.color
+  }).returning('id');
 }
 
 const findAll = () => {
-    return db({t: 'events.tag'})
-        .select([
-            'id',
-            'name',
-            'color'
-        ]);
+  return db({ t: 'events.tag' })
+    .select([
+      'id',
+      'name',
+      'color'
+    ]);
 }
 
 const findById = id => {
-    return findAll().where({id}).first();
+  return findAll().where({ id }).first();
 }
 
 const findByKeyword = keyword => {
-    const pattern = `%${keyword.replace('_', '\\_').replace('%', '\\%')}%`
-    return findAll().whereILike('name', pattern);
+  const pattern = `%${keyword.replace('_', '\\_').replace('%', '\\%')}%`
+  return findAll().whereILike('name', pattern);
 }
 
 module.exports = {
-    createTag,
-    findAll,
-    findById,
-    findByKeyword
+  create,
+  findAll,
+  findById,
+  findByKeyword
 }
