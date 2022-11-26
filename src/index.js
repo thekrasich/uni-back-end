333c5010a880c86999b10266792a31e2db55d156
NebelFox <mashtaliaryaroslav@gmail.com>
2022-11-26 16:49:54 +0200

    Add cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const { initRouters } = require("./routes");
 
@@ -7,6 +8,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
+// app.use(cors());
 
 initRouters(app);
 
