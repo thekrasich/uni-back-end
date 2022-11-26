968f2cbe3598442f054a4af214a5f77ab77cb16c
NebelFox <mashtaliaryaroslav@gmail.com>
2022-11-26 12:45:53 +0200

    Add create and findById to repo/faculty

--- src/services/faculty.js
+++ src/services/faculty.js
@@ -1,10 +1,18 @@
 const facultyRepository = require('../repositories/faculty');
 
+const create = (req, res) => facultyRepository.create(req.body)
+  .then(id => res.send(id));
+
 const findAll = (req, res) => facultyRepository.findAll()
     .then(events => {
         res.send(events);
     });
 
+const findById = (req, res) => facultyRepository.findById(+req.params.id)
+  .then(faculty => res.send(faculty));
+
 module.exports = {
-    findAll
+  create,
+  findAll,
+  findById
 }
