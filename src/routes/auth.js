52c571eca4e4b7b56dc4038dc72c589874c3c2d8
NebelFox <mashtaliaryaroslav@gmail.com>
2022-11-27 14:37:36 +0200

    Fix confused sign-in & sign-up routes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -6,7 +6,7 @@ const { validate, errorHandler } = require("./../middleware");
 const authService = require('../services/auth');
 const { body } = require("express-validator");
 
-router.post('/auth/sign-in',
+router.post('/auth/sign-up',
   body('roleId').isInt().toInt(),
   body('fullName'),
   body('email').isEmail(),
@@ -14,7 +14,7 @@ router.post('/auth/sign-in',
   validate,
   errorHandler(authService.signIn));
 
-router.post('/auth/sign-up',
+router.post('/auth/sign-in',
   body('email').isEmail(),
   body('password'),
   validate,
