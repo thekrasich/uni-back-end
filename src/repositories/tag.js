5d709a6056472829393c1db3bbd89f2b269665d6
NebelFox <mashtaliaryaroslav@gmail.com>
2022-11-27 15:06:28 +0200

    Add update and remove methods to tag repo

--- src/repositories/tag.js
+++ src/repositories/tag.js
@@ -1,12 +1,41 @@
 const { db } = require("./db");
 
+const TABLE = 'events.tag';
+
 const create = ({ name, color }) => {
-  return db('events.tag').insert({ name, color, }, '*')
+  return db(TABLE).insert({ name, color, }, '*')
     .then(([tag]) => tag);
 }
 
-const findAll = ({keyword} = {}) => {
-  return db({ t: 'events.tag' })
+const update = (id, { name, color }) => db(TABLE)
+  .where('id', '=', id)
+  .update({ name, color })
+  .then(n => n > 0 && { id, name, color });
+
+const remove = async id => {
+  const trx = await db.transaction();
+
+  try {
+    const affectedEventsNumber = await trx('events.event_tag')
+      .where('tag_id', '=', id)
+      .del();
+
+    const n = await trx(TABLE)
+      .where('id', '=', id)
+      .del();
+
+    await trx.commit();
+
+    return { found: n > 0, affectedEventsNumber };
+  } catch (e) {
+    await trx.rollback();
+    console.log(e);
+    throw e;
+  }
+}
+
+const findAll = ({ keyword } = {}) => {
+  return db({ t: TABLE })
     .select(['id', 'name', 'color'])
     .modify(b => {
       keyword && b.whereILike('name', `%${keyword}%`);
@@ -19,6 +48,8 @@ const findById = id => {
 
 module.exports = {
   create,
+  update,
+  remove,
   findAll,
   findById
 }
