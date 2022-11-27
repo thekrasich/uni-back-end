47bd1309102ae35db8651c8834cf56e0405b1907
NebelFox <mashtaliaryaroslav@gmail.com>
2022-11-27 15:04:43 +0200

    Add update and remove methods to tag service

--- src/services/tag.js
+++ src/services/tag.js
@@ -5,6 +5,27 @@ const create = async (req, res) => {
   res.status(201).send(tag);
 }
 
+const update = (req, res) => {
+  tagRepo.update(req.params.id, req.body).then(tag => {
+    if (tag) {
+      res.status(204).send(tag)
+    } else {
+      res.status(404).send({ errorMessage: 'Tag not found' })
+    }
+  });
+}
+
+const remove = async (req, res) => {
+  tagRepo.remove(req.params.id)
+    .then(({ found, affectedEventsNumber }) => {
+      if (found) {
+        res.status(200).send({ affectedEventsNumber })
+      } else {
+        res.status(404).send({ errorMessage: 'Tag not found' });
+      }
+    })
+}
+
 const findAll = (req, res) => {
   return tagRepo.findAll(req.body)
     .then(events => res.send(events));
@@ -27,6 +48,8 @@ const findById = (req, res) => {
 
 module.exports = {
   create,
+  update,
+  remove,
   findAll,
   findById
 }
