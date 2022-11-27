35d24e3e669863566875e3e69c08391874e53079
NebelFox <mashtaliaryaroslav@gmail.com>
2022-11-27 14:40:38 +0200

    Enable auth middleware

--- src/middleware.js
+++ src/middleware.js
@@ -6,45 +6,42 @@ const jwtSecret = 'test';
 const AUTH_TOKEN_HEADER = "Authorization";
 const TOKEN_PREFIX = "Bearer";
 
-// const authRole = (minRequiredRole = 1) => (req, res, next) => {
-//   const authHeader = req.header(AUTH_TOKEN_HEADER);
-//
-//   if (!authHeader) {
-//     res.status(401).send({ errorMessage: 'Missed Authorization header' });
-//     return;
-//   }
-//
-//   const authHeaderParts = authHeader.split(' ');
-//   if (authHeaderParts.length !== 2 || TOKEN_PREFIX !== authHeaderParts[0]) {
-//     res.status(401).send({ errorMessage: 'Invalid format of Auth header!' });
-//     return;
-//   }
-//
-//   const jwtToken = authHeaderParts[1];
-//
-//   try {
-//     jwt.verify(jwtToken, jwtSecret);
-//   } catch (e) {
-//     res.status(401).send({ errorMessage: 'Invalid Auth token!' });
-//     return;
-//   }
-//
-//   const { userId, roleId } = jwt.decode(jwtToken);
-//
-//   if (roleId < minRequiredRole) {
-//     res.status(403).send({ errorMessage: 'Your role has not enough rights for such request' });
-//     return;
-//   }
-//
-//   req.user = { id: userId, roleId };
-//
-//   next();
-// }
-//
-// const auth = authRole(1);
+const authRole = (minRequiredRole = 1) => (req, res, next) => {
+  const authHeader = req.header(AUTH_TOKEN_HEADER);
 
-const auth = (req, res, next) => next();
-const authRole = (minRequiredRole) => (req, res, next) => next();
+  if (!authHeader) {
+    res.status(401).send({ errorMessage: 'Missed Authorization header' });
+    return;
+  }
+
+  const authHeaderParts = authHeader.split(' ');
+  if (authHeaderParts.length !== 2 || TOKEN_PREFIX !== authHeaderParts[0]) {
+    res.status(401).send({ errorMessage: 'Invalid format of Auth header!' });
+    return;
+  }
+
+  const jwtToken = authHeaderParts[1];
+
+  try {
+    jwt.verify(jwtToken, jwtSecret);
+  } catch (e) {
+    res.status(401).send({ errorMessage: 'Invalid Auth token!' });
+    return;
+  }
+
+  const { userId, roleId } = jwt.decode(jwtToken);
+
+  if (roleId < minRequiredRole) {
+    res.status(403).send({ errorMessage: 'Your role has not enough rights for such request' });
+    return;
+  }
+
+  req.user = { id: userId, roleId };
+
+  next();
+}
+
+const auth = authRole(1);
 
 const validate = (req, res, next) => {
   const errors = validationResult(req);
